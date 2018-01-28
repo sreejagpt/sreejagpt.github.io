18429b97b6dc4878aba8ecd723d3d9305c63264b
Sreeja Gupta <sreejagpt@gmail.com>
2018-01-28 20:21:14 +1100

    Updates

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'sreejagpt\'s personal blog',
-  },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass' ],
-    
-};
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const IndexPage = () => (
-  <div>
-    <h1>My name is sreejagpt.</h1>
-  </div>
-)
-
-export default IndexPage
