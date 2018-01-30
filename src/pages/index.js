b695162e07eff71cf6f9a8036173448ddde316b3
Sreeja Gupta <sreejagpt@gmail.com>
2018-01-31 00:20:18 +1100

    added logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Link from 'gatsby-link'
+
+import './Pages.scss'
+import linkedin from './assets/linkedin.png'
+import github from './assets/github.png'
+import twitter from './assets/twitter.png'
+import me from './assets/me.png'
 
 const IndexPage = () => (
   <div>
     <h1>My name is sreejagpt.</h1>
-    <p>Find me on LinkedIn or GitHub or IRL!</p>
+    <div className='row'>
+      <a href='//www.linkedin.com/in/sreeja-gupta'><img src={linkedin}/></a>
+      <a href='//https://twitter.com/sreejagpt'><img src={twitter}/></a>
+      <a href='//https://github.com/sreejagpt'><img src={github}/></a>
+      <a href='#'><img src={me}/></a>
+    </div>
   </div>
-)
+  )
 
 export default IndexPage
