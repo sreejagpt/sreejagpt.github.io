b22538fe92918f66e6118b50fc577d33afcb8ea0
Sreeja Gupta <sreejagpt@gmail.com>
2018-02-01 17:31:22 +1100

    landing page with links

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,22 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import './Pages.scss'
-import linkedin from './assets/linkedin.png'
-import github from './assets/github.png'
-import twitter from './assets/twitter.png'
 import me from './assets/me.png'
 
 const IndexPage = () => (
-  <div>
-    <h1>My name is sreejagpt.</h1>
-    <div className='row'>
-      <a href='//www.linkedin.com/in/sreeja-gupta'><img src={linkedin}/></a>
-      <a href='//https://twitter.com/sreejagpt'><img src={twitter}/></a>
-      <a href='//https://github.com/sreejagpt'><img src={github}/></a>
-      <a href='#'><img src={me}/></a>
+  <div className='row'>
+    <a href='#'><img src={me}/></a>
+    <div className='bio'>
+      <h1>sreejagpt.</h1>
+      <h2>Full stack developer.</h2>
+      <div>I like coding, cooking and painting.</div>
+      <div className='inline-icons'>
+        <a href='http://www.linkedin.com/in/sreeja-gupta' className='linkedin' target='_blank'/>
+        <a href='https://twitter.com/sreejagpt/' className='twitter' target='_blank'/>
+        <a href='https://github.com/sreejagpt' className='github' target='_blank'/>
+      </div>
     </div>
   </div>
-  )
+)
 
 export default IndexPage
