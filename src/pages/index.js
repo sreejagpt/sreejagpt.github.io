114ce110d696bd25e9f6499b4d5a65770d960c4d
Sreeja Gupta <sreejagpt@gmail.com>
2018-02-11 22:08:06 +1100

    slidey slidey

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,55 @@
 import React from 'react'
+import classNames from 'classnames'
 import './Pages.scss'
 import AboutMe from '../components/AboutMe'
 import projects from './projects.png'
 import talks from './talks.png'
 
-const IndexPage = () => (
-  <div>
-    <div className='verticalTimeline'>
-      <AboutMe/>
 
-      <div className='projects'>
-        <div className='inline-heading'>
-          <img src={projects}/>
-          <div className='inline-text'>Projects.</div>
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      projects: false,
+      talks: false,
+    }
+  }
+  
+  handleProjectsSelected = () => { 
+    this.setState({ projects: !this.state.projects, talks: false })
+  }
 
-          
+  isProjectsSelected = () => this.state.projects === true;
+
+  handleTalksSelected = () => { 
+    this.setState({ talks: !this.state.talks, projects: false })
+  }
+
+  isTalksSelected = () => this.state.talks === true;
+
+  render() {
+    return (
+      <div>
+        <div className='verticalTimeline'>
+          <AboutMe/>
+
+          <div className={ classNames({ projects: true, selected: this.isProjectsSelected() }) } onClick={ this.handleProjectsSelected }>
+
+            <div className='inline-heading'>
+              <img src={projects}/>
+              <div className='inline-text'>Projects.</div> 
+            </div>
+          </div>
+          <div className={ classNames({ talks: true, selected: this.isTalksSelected() }) } onClick={ this.handleTalksSelected }>
+            <img src={talks}/>
+            <div className='inline-text'>Talks.</div>
+          </div>
         </div>
       </div>
+    );
+  }
 
-      <div className='talks'>
-        <img src={talks}/>
-        <div className='inline-text'>Talks.</div>
-      </div>
-    </div>
-  </div>
-)
+}
+
 
 export default IndexPage
