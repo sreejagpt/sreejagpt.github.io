534fd5ea1c8fb6b37f2b4263defff54b7ecd41af
Sreeja G <sreejagpt@gmail.com>
2018-07-14 15:30:14 +1000

    sreeja/michael add left nav

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Navigation.module.scss';
+
+const Navigation = ({ navItems }) => {
+   const navElements = navItems.map((navItem) => <a href={navItem.url}>{navItem.text}</a>);
+
+   return (
+
+    <div className={styles.root}>
+      { navElements }
+    </div>
+  );
+};
+
+Navigation.propTypes = {
+  navItems: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string,
+    url: PropTypes.string,
+  })),
+};
+
+export default Navigation;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import styles from './Pages.module.scss';
+import Navigation from '../components/Navigation';
 import AboutMe from '../components/AboutMe';
-import projects from './assets/projects.png';
-import talks from './assets/talks.png';
-import metamorph from './assets/metamorph.png';
-import happybutton from './assets/happybutton.png';
 
 const IndexPage = () => (
   <div>
+    <Navigation navItems={[{text: 'About Me'}, {text: 'Michael is awesome'}]}/>
     <div className={styles.verticalTimeline}>
       <AboutMe />
     </div>
   </div>
 );
-
-const projectDetails = [
-  {
-    heading: 'Metamorf',
-    subtitle:
-      'A machine learning smart bridge to help producers find original Australian content to adapt for the screen',
-    awards: 'Winner, Arts/Screen Hack 2017',
-    image: metamorph,
-    url: 'https://github.com/sreejagpt/artscreenhack',
-  },
-  {
-    heading: 'The Happy Button',
-    subtitle:
-      'An app with a button to view only good news using the ABC News API and sentiment analysis',
-    awards: 'User Favourite, ABS Data Hackathon 2016',
-    image: happybutton,
-    url: 'https://github.com/sreejagpt/ABCGoodNews',
-  },
-];
 
 export default IndexPage;
